70267ed1ba137b6dd34cfc12d54e0e70d49712e3
ROMEROLUIS15 <luisc@example.com>
2026-06-05 12:58:18 -0400

    Revert "Merge pull request #20 from ROMEROLUIS15/chore/pin-langchain-unblock-deps"
    This reverts commit 50e80e1291938f9f331831c32f8f8b7b52606519, reversing
    changes made to 67f93a6eff175cc57e45995382103dc28b33921a.

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,16 +2,6 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.+(test|spec).+(js)', '**/?(*.)+(spec|test).+(js)'],
-  // Transpila JS con babel-jest (usa backend/babel.config.js).
-  transform: {
-    '^.+\\.[cm]?jsx?$': 'babel-jest',
-  },
-  // Por defecto Jest ignora node_modules al transformar. Excluimos de ese
-  // "ignore" al ecosistema langchain (y deps ESM-only que arrastra) para que
-  // SÍ se transpilen a CommonJS y Jest pueda importarlos.
-  transformIgnorePatterns: [
-    '/node_modules/(?!(@langchain|langchain|langsmith|@cfworker|js-tiktoken|uuid|p-queue|p-timeout|p-retry|eventemitter3|retry|mustache|decamelize|camelcase|@ungap)/)',
-  ],
   collectCoverageFrom: [
     'src/**/*.{js,jsx}',
     '!src/**/*.mock.js',
